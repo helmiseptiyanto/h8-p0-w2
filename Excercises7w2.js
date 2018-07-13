315f7a9badb52bc4077db47131b0b863c10d12e4
Helmi Septiyanto <mamanghelmy@gmail.com>
2018-07-13 17:19:39 +0700

    Ini Tugas Week 2

--- Excercises7w2.js
+++ Excercises7w2.js
@@ -1,1 +1,28 @@
 //Excercises 7 Week 2
+
+// 1. Menyusun Barisan Bintang
+for(var rows1 = 1; rows1 <= 5; rows1++) {
+  if(rows1 %1==0) {
+  console.log('*')
+  }
+ }
+
+ // 2. Menyusun Barisan Bintang Dengan Nested Looping
+
+ var rows1 = 5
+ for(var i = 0; i < 5; i++) {
+   var temp = ''
+   for(var j = 0; j <  5; j++) {
+     temp += '*';
+   }
+   console.log(temp)
+ }
+
+ // 3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+
+ var rows3 = 5;
+     var temp = ''
+     for(var i = 0; i < rows3; i++) {
+       temp += '*';
+       console.log(temp)
+   }
